273caf63c631fd9c58d55453e0b091af0db8c7f0
Maxime <mlacassegermain@cmaisonneuve.qc.ca>
2025-02-04 15:58:11 -0500

    Ajouter routes base

--- index.js
+++ index.js
@@ -1,13 +1,53 @@
-const http = require("http");
+// Importations des librairies
+const express = require("express");
 const dotenv = require("dotenv");
+const path = require("path");
+
+const serveur = express();
 dotenv.config();
 
-const serveur = http.createServer((req, res) => {
-    if (req.method == "GET" && req.url == "/") {
-        res.end("Accueil");
-    } else {
-        res.end("Patate 2");
-    }
+//Permettre l'accès au dossier
+const dossierPublic = path.join(__dirname, "public");
+serveur.use(express.static(dossierPublic));
+
+//Middleware
+function authentifier(req, res, next) {
+    console.log("Authentification en cours");
+    next();
+}
+
+//Routes
+/**
+ * Route servant à récuperer tous les films de la base de données
+ */
+serveur.get("/films", authentifier, (req, res) => {
+    return res.json({ msg: "ok" });
+});
+
+serveur.get("/films/:id", (req, res) => {
+    return res.json({ msg: "ok" });
+});
+
+serveur.post("/films", (req, res) => {
+    return res.json({ msg: "ok" });
+});
+
+serveur.post("/films/initialiser", authentifier, (req, res) => {
+    return res.json({ msg: "ok" });
+});
+
+serveur.put("/films/:id", (req, res) => {
+    return res.json({ msg: "ok" });
+});
+
+serveur.delete("/films/:id", (req, res) => {
+    return res.json({ msg: "ok" });
+});
+
+//Ressources 404
+serveur.use((req, res) => {
+    res.statusCode = 404;
+    return res.status(404).json({ msg: "Ressource non trouvée" });
 });
 
 serveur.listen(process.env.PORT, () => {
