54bfdc69aa37f8060b92651d613856f007cfc033
Maxime <mlacassegermain@cmaisonneuve.qc.ca>
2025-02-10 10:34:23 -0500

    Connexion a la base de donnees

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const path = require("path");
-
+const db = require("./config/db");
 const serveur = express();
 dotenv.config();
 
@@ -20,8 +20,26 @@ function authentifier(req, res, next) {
 /**
  * Route servant à récuperer tous les films de la base de données
  */
-serveur.get("/films", authentifier, (req, res) => {
-    return res.json({ msg: "ok" });
+serveur.get("/films", authentifier, async (req, res) => {
+    try {
+        // const films = require("./data/filmsDepart.js");
+        const films = [];
+
+        const docRefs = await db.collection("films").get();
+
+        docRefs.forEach((doc) => {
+            const film = { id: doc.id, ...doc.data() };
+            films.push(film);
+        });
+
+        if (films.length == 0) {
+            return res.status(404).json({ msg: "Aucun film trouvé" });
+        }
+
+        return res.json(films);
+    } catch (erreur) {
+        return res.status(500).json({ msg: "Une erreur est survenue" });
+    }
 });
 
 serveur.get("/films/:id", (req, res) => {
@@ -32,8 +50,22 @@ serveur.post("/films", (req, res) => {
     return res.json({ msg: "ok" });
 });
 
-serveur.post("/films/initialiser", authentifier, (req, res) => {
-    return res.json({ msg: "ok" });
+serveur.post("/films/initialiser", (req, res) => {
+    try {
+        const films = require("./data/filmsDepart");
+
+        //TODO: Vérifier si le film est déjà dans la base de données
+
+        films.forEach(async (film) => {
+            await db.collection("films").add(film);
+        });
+
+        return res.status(201).json({ msg: "Base de données initialisée" });
+    } catch (erreur) {
+        console.log(erreur);
+
+        return res.status(500).json({ msg: "Une erreur est survenue" });
+    }
 });
 
 serveur.put("/films/:id", (req, res) => {
