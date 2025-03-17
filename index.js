11745158f261c6fdb1a0439004eb8c0e6ef765f7
Maxime <mlacassegermain@cmaisonneuve.qc.ca>
2025-03-17 09:11:53 -0400

    modif serveur

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Importations des librairies
-const serveur = require("./serveur");
+const { serveur } = require("./serveur");
 
-serveur.listen(process.env.PORT||5000, () => {
+serveur.listen(process.env.PORT || 5000, () => {
     console.log(`le serveur est démarré sur le port ${process.env.PORT}`);
 });
